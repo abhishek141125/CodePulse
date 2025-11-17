a77361be115f79373cd022f07c6ca1b11404bed5
abhishek1411-ai <abhishekdevmurari1411@gmail.com>
2025-11-17 14:28:36 +0530

    17-11

---
+++ CodePulse/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Header from './componants/layout/Header'
+import Home from './home'
+import { ToastContainer, toast } from 'react-toastify'
+import About from './About'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+export default function App() {
+  const to = () => {
+    toast.warning('warning')
+    toast.error('error')
+    toast.success('success')
+    toast.info('info')
+    
+    
+  }
+
+  return (
+    <div>
+      <div className="btn-box">
+        <button onClick={to} className="btn">click me</button>
+      </div>
+
+      <ToastContainer/>
+
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
---
+++ CodePulse/src/componants/layout/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/image/logo.jpg'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img src={logo} alt="" />
+      </div>
+
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </div>
+  )
+}
